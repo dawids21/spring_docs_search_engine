b2d61b484074f2feb2ebef61466f5d80eecfb405
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-15 16:04:47 +0100

    create and configure collector

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/gocolly/colly/v2"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	url := "https://docs.spring.io/spring-framework/docs/current/javadoc-api/allclasses-noframe.html"
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("docs.spring.io"),
+	)
+
+	c.Limit(&colly.LimitRule{
+		Delay:       3 * time.Second,
+		RandomDelay: 1 * time.Second,
+		DomainGlob:  "docs.spring.io/*",
+	})
+
 }
