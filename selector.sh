bce4814a3e9ed380a29117056bea86bfb909db8a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-15 20:36:48 +0100

    modify selector to choose project

--- selector.sh
+++ selector.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-KEY=$(awk 'BEGIN { FS = "=" } ; { print $1 }' scraper/file.txt | rofi -dmenu -p "Choose class: " -i -no-custom)
-LINK=$(grep -oP "^$KEY=\K.+" scraper/file.txt)
 
-[[ ! -z $LINK ]] && qutebrowser $LINK
+PROJECT=$(ls classes_urls | awk 'BEGIN { FS = "." } ; { print $1 }' | rofi -dmenu -p "Choose project: " -i -no-custom)
+
+[[ -z $PROJECT ]] && exit
+
+KEY=$(awk 'BEGIN { FS = "=" } ; { print $1 }' classes_urls/$PROJECT.txt | rofi -dmenu -p "Choose class: " -i -no-custom)
+LINK=$(grep -oP "^$KEY=\K.+" classes_urls/$PROJECT.txt)
+
+[[ -z $LINK ]] && exit
+
+qutebrowser $LINK
