ca22ef5361c64c4ecf18f80173eadbecafdc5fad
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-20 16:21:47 +0100

    use absolute path of classes_urls folder

--- selector.sh
+++ selector.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-PROJECT=$(ls classes_urls | awk 'BEGIN { FS = "." } ; { print $1 }' | rofi -dmenu -p "Choose project: " -i -no-custom)
+CLASSES_DIR=~/Files/Programming/My_programs/spring_docs_scraper/classes_urls
+
+PROJECT=$(ls $CLASSES_DIR | awk 'BEGIN { FS = "." } ; { print $1 }' | rofi -dmenu -p "Choose project: " -i -no-custom)
 
 [[ -z $PROJECT ]] && exit
 
-KEY=$(awk 'BEGIN { FS = "=" } ; { print $1 }' classes_urls/$PROJECT.txt | rofi -dmenu -p "Choose class: " -i -no-custom)
-LINK=$(grep -oP "^$KEY=\K.+" classes_urls/$PROJECT.txt)
+KEY=$(awk 'BEGIN { FS = "=" } ; { print $1 }' $CLASSES_DIR/$PROJECT.txt | rofi -dmenu -p "Choose class: " -i -no-custom)
+LINK=$(grep -oP "^$KEY=\K.+" $CLASSES_DIR/$PROJECT.txt)
 
 [[ -z $LINK ]] && exit
 
